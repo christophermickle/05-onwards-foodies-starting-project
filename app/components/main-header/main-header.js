54fd0963f6360163f6d612873268e90f4b079668
Christopher Mickle <cmickle.dev@gmail.com>
2023-12-13 19:15:28 -0600

    Chore: Added untracked file from refactor, --linting

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -11,17 +11,17 @@ export default function MainHeader() {
     <>
       <MainHeaderBackground />
       <header className={classes.header}>
-        <Link className={classes.logo} href='/'>
-          <Image src={logoImg} alt='A plate with food on it.' />
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it." />
           NextLevel Food
         </Link>
         <nav className={classes.nav}>
           <ul>
             <li>
-              <NavLink href='/meals'>Browse Meals</NavLink>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <NavLink href='/community'>Foodies Community</NavLink>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
