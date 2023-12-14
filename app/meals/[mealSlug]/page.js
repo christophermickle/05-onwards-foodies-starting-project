66a45c39bd7ba32b81d2b8244598eb176591d46d
Christopher Mickle <cmickle.dev@gmail.com>
2023-12-14 01:47:00 -0600

    Refactor meal details page and add CSS styles

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,35 @@
-function MealDetails() {
+import  Image  from "next/image";
+import classes from "./meal-details.module.css";
+import { getMeal } from "@/app/lib/meals";
+
+function MealDetails({params}) {
+  const meal = getMeal(params.mealSlug)
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br/>')
   return (
-    <div>
-      <h1>Meal Details</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title}/>
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            {" "}
+            created by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html:meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
 
